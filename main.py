f7ca51523eae33c5e454a4880fcd8c12ff3bbbed
Peter Moise <moise.p@northeastern.edu>
2024-07-09 21:58:23 +0000

    Added two functions for querying and parsing api data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
+from petfinder import api_query_response, chosen_post_data
 
 app = Flask(__name__)
-proxied = FlaskBehindProxy(app)
 
 @app.route("/")
 def replace_here():
