a06880a163d1d777f34a32a19181cab9b54f79dd
Tiffanie Kwakye <tiffanie.n.kwakye@gmail.com>
2024-07-10 15:19:04 +0000

    non style commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
@@ -10,20 +10,21 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    if request.is_json:
+        data = request.get_json()
+        question1 = data.get('question1')
+        question2 = data.get('question2')
+        question3 = data.get('question3')
 
-    question1 = request.form.get('question1')
-    question2 = request.form.get('question2')
-    question3 = request.form.get('question3')
+        stored_data.append({
+            'question1': question1,
+            'question2': question2,
+            'question3': question3
+        })
 
-
-    stored_data.append({
-        'question1': question1,
-        'question2': question2,
-        'question3': question3
-
-    })
-
-    return redirect(url_for('index')) 
+        return jsonify({'message': 'Data submitted successfully!'}), 200
+    else:
+        return jsonify({'message': 'Invalid data format.'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0" )
