33aff4bae197149365fa9cd2bcf5b2804996a2d7
pmoise <petercmoise@gmail.com>
2024-07-18 16:06:10 -0400

    fixing conflict

--- petfinder.py
+++ petfinder.py
@@ -8,44 +8,50 @@ headers = {
   'Accept': 'application/json',
 }
 
-# In the form there should be validation for the correct values
+hypo_breeds = ['siberian', 'balinese', 'oriental shorthair', 'russian blue', 'devon rex',
+                'poodle', 'bichon frise', 'portuguese water dog', 'maltese', 'schnauzer',
+                  'rex rabbit', 'angora rabbit', 'mini rex']
+
 
 """
- Returns a list of dog posts from the user's input requests 
+ Returns a list of pets(dogs,cats,rabbits,bird) posts from the user's input requests 
 """
-def api_query_response(city_or_zip, geo_range, sex, age, special_needs):
-  url = f'{base_url}pet_search?key={api_key}&v=3&output=json'
-  resulting_pets = []
-  params = {
-  'species':'dog',
-  'city_or_zip': f'{city_or_zip}',
-  'geo_range': f'{geo_range}',
-  'sex': f'{sex}',
-  'age': f'{age}',
-  'special_needs': f'{special_needs}'
-  }
+def api_query_response(species, city_or_zip, geo_range, sex, age, special_needs, size, allergies):
+    url = f'{base_url}pet_search?key={api_key}&v=3&output=json'
+    resulting_pets = []
+    params = {
+        'species': f'{species}',
+        'city_or_zip': f'{city_or_zip}',
+        'geo_range': f'{geo_range}',
+        'sex': f'{sex}',
+        'age': f'{age}',
+        'special_needs': f'{special_needs}',
+        'pet_size_range_id': f'{size}'
+    }
 
-  response = requests.request("GET", url, headers=headers, data=payload, params=params)
+    response = requests.request("GET", url, headers=headers, data=payload, params=params)
 
-  try:
-    list_of_pet_dictionaries = response.json()["pets"]
-  except KeyError:
-    print("Try different query")
-  else:
-    for dictionary in list_of_pet_dictionaries:
-      resulting_pets_dictionary = {}
-      resulting_pets_dictionary['pet_id'] = dictionary.get('pet_id')
-      resulting_pets_dictionary['pet_name'] = dictionary.get('pet_name')
-      resulting_pets_dictionary['primary_breed'] = dictionary.get('primary_breed')
-      resulting_pets_dictionary['secondary_breed'] = dictionary.get('secondary_breed')
-      resulting_pets_dictionary['sex'] = dictionary.get('sex')
-      resulting_pets_dictionary['age'] = dictionary.get('age')
-      resulting_pets_dictionary['size'] = dictionary.get('size')
-      resulting_pets_dictionary['photo_link'] = dictionary.get('results_photo_url')
-      resulting_pets_dictionary['large_photo_link'] = dictionary.get('large_results_photo_url')
-      resulting_pets.append(resulting_pets_dictionary)
-  finally:
-    return resulting_pets
+    try:
+        list_of_pet_dictionaries = response.json()["pets"]
+    except KeyError:
+        print("Try different query")
+    else:
+        for dictionary in list_of_pet_dictionaries:
+            resulting_pets_dictionary = {}
+            if not allergies or dictionary.get('primary_breed').lower() in hypo_breeds:
+                resulting_pets_dictionary['pet_id'] = dictionary.get('pet_id')
+                resulting_pets_dictionary['pet_name'] = dictionary.get('pet_name')
+                resulting_pets_dictionary['primary_breed'] = dictionary.get('primary_breed')
+                resulting_pets_dictionary['secondary_breed'] = dictionary.get('secondary_breed')
+                resulting_pets_dictionary['sex'] = dictionary.get('sex')
+                resulting_pets_dictionary['age'] = dictionary.get('age')
+                resulting_pets_dictionary['size'] = dictionary.get('size')
+                resulting_pets_dictionary['photo_link'] = dictionary.get('results_photo_url')
+                resulting_pets.append(resulting_pets_dictionary)
+    finally:
+        return resulting_pets
+
+#print(api_query_response('rabbit', '32703', '100', '', 'young', '0', '', False))
 
 """
  Returns data about a specific dog by their pet_id
@@ -61,6 +67,3 @@ def chosen_post_data(pet_id):
     print("No pets by that ID")
   else:
     return chosen_pet_dictionary
-  
-# print(api_query_response('32703', '100', 'm', 'young', 1))
-# print(chosen_post_data('27879966'))
